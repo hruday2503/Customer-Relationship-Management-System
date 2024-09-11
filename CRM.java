8725179ef830351d9997d22f55ab917a1405c0ec
hruday2503 <151981824+hruday2503@users.noreply.github.com>
2024-09-11 08:38:48 +0530

    Create CRM.java

---
+++ CRM.java
@@ -0,0 +1,47 @@
+package crm;
+
+import DAO.DBConnection;
+import Utilities.LogFiles;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import Utilities.RBMain;
+import Utilities.WindowSizing;
+import static Utilities.WindowSizing.prepStage;
+import java.io.IOException;
+
+
+public class CRM extends Application {
+    
+    @Override
+    public void start(Stage stage) throws IOException {
+        
+        // Setup ResourceBundle
+        RBMain.setRb();
+        
+        prepStage(stage);
+        Parent root = FXMLLoader.load(getClass().getResource("/View/LoginScreen.fxml"),RBMain.getRb());
+        Scene scene = new Scene(root,WindowSizing.getWidth(stage),WindowSizing.getHeight(stage));
+        scene.getStylesheets().add("Resources/generalStylesheet.css");
+        root.requestFocus(); // Prevent username textfield from being focused upon initialization
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        LogFiles.setupLogger();
+        DBConnection.startConnection(); // Connect to MySQL DB
+        //TestConditions.createTestCustomer(); //***AAW: TESTING
+        launch(args);
+        //TestConditions.cleanUp(); //***AAW: TESTING
+        DBConnection.closeConnection(); // Disconnect from MySQL DB
+        
+    }
+    
+}
